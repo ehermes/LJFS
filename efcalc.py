769941275b12e2bc411db0553a84c9285ebd3af8
Eric Hermes <ehermes@chem.wisc.edu>
2012-06-04 12:10:15 -0500

    beginning of chi sq functions

--- efcalc.py
+++ efcalc.py
@@ -8,6 +8,7 @@ import readkey
 import analyze
 
 fileprefix = sys.argv[1]
+optatom = int(sys.argv[2])
 
 arcname = fileprefix + '.arc'
 keyname = fileprefix + '.key'
@@ -34,47 +35,82 @@ prm_ep = keydata[1]
 prm_ch = keydata[2]
 conv = keydata[3]
 
-def eandf(atomnum,n,x,y,z,atomtypes,prm_sigma,prm_ep,prm_ch,conv):
+def chisq(e,s):
+    epsilon = prm_ep
+    sigma = prm_sigma
+    charge = prm_ch
+    epsilon[atomtype[optatom - 1]] = e
+    sigma[atomtype[optatom - 1]] = s
+    [energy,force] = e_f(epsilon,sigma,charge)
+
+def gradchisq(e,s):
+    epsilon = prm_ep
+    sigma = prm_sigma
+    charge = prm_ch
+    epsilon[atomtype[optatom - 1]] = e
+    sigma[atomtype[optatom - 1]] = s
+    [denergy,dforce] = de_df(epsilon,sigma,charge)
+
+def e_f(epsilon=prm_ep,sigma=prm_sigma,charge=prm_ch):
     elist = []
-    delist = []
     flist = []
-    dflist = []
     for i in xrange(n):
     energy = 0.0
-    gradenergy = 0.0
     force = np.array([0.0,0.0,0.0])
-    gradforce = np.array([0.0,0.0,0.0])
         for j in xrange(len(x[i])):
             if j != atomnum and j+1 not in newbonds[i][j]:
-                ep1 = prm_ep[atomtype[i][atomnum]]
-                ep2 = prm_ep[atomtype[i][j]]
-                sig1 = prm_sigma[atomtype[i][atomnum]]
-                sig2 = prm_sigma[atomtype[i][j]]
+                ep1 = epsilon[atomtype[i][atomnum]]
+                ep2 = epsilon[atomtype[i][j]]
+                sig1 = sigma[atomtype[i][atomnum]]
+                sig2 = sigma[atomtype[i][j]]
                 r = np.array([x[i][atomnum]-x[i][j],y[i][atomnum]-y[i][j],
                     z[i][atomnum]-z[i][j]])
-                q1 = prm_ch[atomtype[i][atomnum]]
-                q2 = prm_ch[atomtype[i][j]]
+                q1 = charge[atomtype[i][atomnum]]
+                q2 = charge[atomtype[i][j]]
                 if ep1 != 0 and ep2 != 0:
                     energy += analyze.elj12(ep1,ep2,sig1,sig2,r)
                     energy += analyze.elj12(ep1,ep2,sig1,sig2,r)
-                    gradenergy += analyze.ddepelj12(ep1,ep2,sig1,sig2,r)
-                    gradenergy += analyze.ddsigelj12(ep1,ep2,sig1,sig2,r)
-                    gradenergy += analyze.ddepelj6(ep1,ep2,sig1,sig2,r)
-                    gradenergy += analyze.ddsigelj6(ep1,ep2,sig1,sig2,r)
                     force += analyze.flj12(ep1,ep2,sig1,sig2,r)
                     force += analyze.flj6(ep1,ep2,sig1,sig2,r)
-                    gradforce += analyze.ddepflj12(ep1,ep2,sig1,sig2,r)
-                    gradforce += analyze.ddsigflj12(ep1,ep2,sig1,sig2,r)
-                    gradforce += analyze.ddepflj6(ep1,ep2,sig1,sig2,r)
-                    gradforce += analyze.ddsigflj6(ep1,ep2,sig1,sig2,r)
                 if q1 != 0 and q2 != 0:
                     energy += analyze.ecoul(conv,q1,q2,r)
-                    gradenergy += analyze.ddqecoul(conv,q1,q2,r)
                     force += analyze.fcoul(conv,q1,q2,r)
-                    gradforce += ddfcoul(conv,q1,q2,r)
         elist.append(energy)
-        delist.append(gradenergy)
         flist.append(force)
-        dflist.append(gradforce)
+    return [elist,flist]
 
-    return [elist,delist,flist,dflist]
+def de_df(epsilon=prm_ep,sigma=prm_sigma,charge=prm_ch):
+    delist = []
+    dflist = []
+    for i in xrange(n):
+    dedep = 0.0
+    dedsig = 0.0
+    dedq = 0.0
+    dfdep = np.array([0.0,0.0,0.0])
+    dfdsig = np.array([0.0,0.0,0.0])
+    dfdq = np.array([0.0,0.0,0.0])
+        for j in xrange(len(x[i])):
+            if j != atomnum and j+1 not in newbonds[i][j]:
+                ep1 = epsilon[atomtype[i][atomnum]]
+                ep2 = epsilon[atomtype[i][j]]
+                sig1 = sigma[atomtype[i][atomnum]]
+                sig2 = sigma[atomtype[i][j]]
+                r = np.array([x[i][atomnum]-x[i][j],y[i][atomnum]-y[i][j],
+                    z[i][atomnum]-z[i][j]])
+                q1 = charge[atomtype[i][atomnum]]
+                q2 = charge[atomtype[i][j]]
+                if ep1 != 0 and ep2 != 0:
+                    dedep += analyze.ddepelj12(ep1,ep2,sig1,sig2,r) + \
+                            analyze.ddepelj6(ep1,ep2,sig1,sig2,r)
+                    dedsig += analyze.ddsigelj12(ep1,ep2,sig1,sig2,r) + \
+                            analyze.ddsigelj6(ep1,ep2,sig1,sig2,r)
+                    dfdep += analyze.ddepflj12(ep1,ep2,sig1,sig2,r) + \
+                            analyze.ddepflj6(ep1,ep2,sig1,sig2,r)
+                    dfdsig += analyze.ddsigflj12(ep1,ep2,sig1,sig2,r) + \
+                            analyze.ddsigflj6(ep1,ep2,sig1,sig2,r)
+                if q1 != 0 and q2 != 0:
+                    dedq += analyze.ddqecoul(conv,q1,q2,r)
+                    dfdq += ddfcoul(conv,q1,q2,r)
+        delist.append(np.array([dedep,dedsig,dedq]))
+        dflist.append(np.array([dfdep,dfdsig,dfdq]))
+    return [delist,dflist]
